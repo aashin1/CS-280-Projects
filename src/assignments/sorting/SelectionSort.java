da6debd872e6318c54152e0373a9fe9204a9555c
aashin1 <katwadaashin@gmail.com>
2026-09-06 22:17:23 -0400

    generalized selectionsort to work with any type of comparable<t>

--- src/assignments/sorting/SelectionSort.java
+++ src/assignments/sorting/SelectionSort.java
@@ -1,27 +1,27 @@
 package assignments.sorting;
 
-public class SelectionSort extends SortingAlgorithm {
+public class SelectionSort<T extends Comparable<T>> extends SortingAlgorithm<T> {
 /**
  * post condition: array is sorted in ascending order
  * @param array an array of integers
  */
 
-    public void sort(Integer[] array) {
+    public void sort(T[] array) {
         for (int i = 0; i <array.length - 1; i++) { // this is the position we are currently filling in (i)
             int minIndex = i;
             for (int j = i + 1; j < array.length; j++) {
-                if (array[j] < array[minIndex]) {
+                if (array[j].compareTo(array[minIndex])<0) {
                     minIndex = j;
                 }
             }
-            Integer temp = array[i];
+            T temp = array[i];
             array[i] = array[minIndex];
             array[minIndex] = temp;
         }
 
     }
     public static void main(String[] args) {
-        SortingAlgorithm.validate(new SelectionSort());
+        SortingAlgorithm.validate(new SelectionSort<Integer>());
         System.out.println("selection sort has passed all tests.");
     }
 }
